fe3ca0c46553a9cf0de58569932b3699a0e7b8f0
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-08-04 13:06:30 +0530

    patterns

---
+++ step-1 Learn The Basics/Lec2/Pattern.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+using namespace std;
+void pattern1(int n){
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void pattern2(int n){
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<=i; j++){
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void pattern3(int n){
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<=i; j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void pattern4(int n){
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<=i; j++){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void pattern5(int n){
+    for(int i = 0; i<n; i++){
+        for(int j = n; j>i ;j--){
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void pattern6(int n){
+    for(int i = 0; i<n; i++){
+        for(int j = n; j>=i; j--){
+            cout<<n-j+1<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+main()
+{
+    int n = 5;
+    // pattern1(n);
+    // pattern2(n);
+    // pattern3(n);
+    // pattern4(n);
+    // pattern5(n);
+    pattern6(n);
+    return 0;
+}
