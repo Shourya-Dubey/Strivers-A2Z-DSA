f5034c61d9a5c64baa5da7f8dee17fab4b65a06f
Shourya-Dubey <shourya.dubey1999@gmail.com>
2024-09-23 00:48:44 +0530

    Factorial by Recursion

--- step-1 Learn The Basics/Lec5/Rec4.cpp
+++ step-1 Learn The Basics/Lec5/Rec4.cpp
@@ -13,19 +13,38 @@ void reverseArrTwoPointer(int st, int ls, int arr[]){
     reverseArrTwoPointer(st+1, ls-1, arr);
 }
 
+bool palindrom(int i, string &s){
+    if(i >= (s.size()-1) / 2) return true;
+    else if(s[i] != s[s.size()-i-1]) return false;
+    return palindrom(i+1, s);
+}
+
+int fibonaci(int n){
+    if(n <= 1) return n;
+    int last = fibonaci(n - 1);
+    int sLast = fibonaci(n - 2);
+    return last + sLast; 
+}
+
 main()
 {
-    int n;
-    cout<<"Enter a number for size of array ";
-    cin>>n;
+    // int n;
+    // cout<<"Enter a number for size of array ";
+    // cin>>n;
 
-    int arr[n];
-    for(int i = 0; i<n; i++) cin>>arr[i];
+    // int arr[n];
+    // for(int i = 0; i<n; i++) cin>>arr[i];
     // reverseArr(0, arr, n);
     // for(int i = 0; i<n; i++) cout<<arr[i]<<" ";
 
-    reverseArrTwoPointer(0, n-1, arr);
-    for (int i = 0; i < n; i++) cout << arr[i] << " ";
+    // reverseArrTwoPointer(0, n-1, arr);
+    // for (int i = 0; i < n; i++) cout << arr[i] << " ";
+
+    // string s = "madsm";
+    // cout<<palindrom(0, s);
+
+    int f = 4;
+    cout<<fibonaci(f);
 
     return 0;
 }
