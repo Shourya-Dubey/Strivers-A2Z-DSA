66fc90a1ebd9c8a628d2775c40946f5dd18d5373
Shourya-Dubey <shourya.dubey1999@gmail.com>
2025-01-24 21:30:45 +0530

    Insertion in LL

--- step-5 LinkedList/lec1_1DlinkedList/Insertion.cpp
+++ step-5 LinkedList/lec1_1DlinkedList/Insertion.cpp
@@ -40,16 +40,92 @@ void printLL(Node *head)
     }
     cout << endl;
 }
-Node *insertion(Node *head, int val)
+Node *insertionAtHead(Node *head, int val)
 {
     Node *temp = new Node(val, head);
     return temp;
+}
+Node *insertionAtTail(Node *head, int val)
+{
+    if(head == NULL)
+    {
+        return new Node(val);
+    }
+    Node *temp = head;
+    while(temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    Node *newNode = new Node(val);
+    temp->next = newNode;
+    return head;
+}
+Node *insertionKthPosition(Node *head, int val, int ele)
+{
+    if(head == NULL)
+    {
+        if(ele == 1)
+        {
+            return new Node(val);
+        }
+    }
+    if(ele == 1)
+    {
+        // Node *temp = new Node(val);
+        // temp->next = head;
+        return new Node(val, head);
+    }
+    int count = 0;
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        count++;
+        if(count == ele-1)
+        {
+            Node *newNode = new Node(val);
+            newNode->next = temp->next;
+            temp->next = newNode;
+            break;
+        }
+        temp = temp->next;
+    }
+    return head;
+}
+Node *insertBeforeValue(Node *head, int k, int val)
+{
+    if(head == NULL)
+    {
+        return NULL;
+    }
+    if(head->data == val)
+    {
+        return new Node(k, head);
+    }
+
+    Node *temp = head;
+    while(temp->next != NULL)
+    {
+        if(temp->next->data == val)
+        {
+            Node *newNode = new Node(k, temp->next);
+            temp->next = newNode;
+            break;
+        }
+        temp = temp->next;
+    }
+    return head;
 }
 main()
 {
     vector<int> arr{12, 2, 3, 4, 5};
     Node *head = convert2LL(arr);
-    head = insertion(head, 300);
+    head = insertionAtHead(head, 300);
+    printLL(head);
+    head = insertionAtTail(head, 100);
+    printLL(head);
+    head = insertionKthPosition(head, 8, 3);
+    printLL(head);
+    head = insertBeforeValue(head, 50, 8);
     printLL(head);
     return 0;
 }
