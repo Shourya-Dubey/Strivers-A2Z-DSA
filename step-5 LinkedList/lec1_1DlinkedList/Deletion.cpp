b8264f847c9453da0a14c71228b91e2dec4c174a
Shourya-Dubey <shourya.dubey1999@gmail.com>
2025-01-24 18:43:15 +0530

    Delete Kth

--- step-5 LinkedList/lec1_1DlinkedList/Deletion.cpp
+++ step-5 LinkedList/lec1_1DlinkedList/Deletion.cpp
@@ -60,13 +60,43 @@ Node* deleteTail(Node* head)
     temp->next = nullptr;
     return head;
 }
-main() {
+Node* deleteK(Node* head, int k)
+{
+    if(head == NULL) return head;
+    if(k == 1)
+    {
+        Node* temp = head;
+        head = head->next;
+        free(temp);
+        return head;
+    }
+    int count = 0;
+    Node* temp = head;
+    Node* prev = NULL;
+    while (temp != NULL)
+    {
+        count++;
+        if(count == k)
+        {
+            prev->next = prev->next->next;
+            free(temp);
+            break;
+        }
+        prev = temp;
+        temp = temp->next;
+    }
+    return head;
+}
+main()
+{
     vector<int> arr{12, 2, 3, 4, 5};
     Node* head = convert2LL(arr);
     printLL(head);
     head = deleteHead(head);
     printLL(head);
     head = deleteTail(head);
+    printLL(head);
+    head = deleteK(head, 2);
     printLL(head);
 return 0;
 }
